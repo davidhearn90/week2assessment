a501b1ee7331a6b3daf113ec59d33a6f049135d6
David Hearn <davidhearn90@gmail.com>
2015-04-29 08:57:10 -0700

    finished quiz

---
+++ main.js
@@ -0,0 +1,13 @@
+$( document ).ready(function(){
+    $( ".dan" ).on( "click", function() {
+        alert( "hey you clicked me" );
+    });
+ 
+});
+
+$( document ).ready(function(){
+    $( "#iwao" ).on( "click", function() {
+        $( "#iwao" ).hide();
+    });
+ 
+});
